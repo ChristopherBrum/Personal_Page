0625a6c979971e3478abea12fcc323c1fe2ea824
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-03-10 22:08:02 -0500

    starry background and title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
-import Idealogy from './components/Idealogy.js'
 import Intro from './components/Intro.js';
-import AboutMe from './components/AboutMe.js'
+import Idealogy from './components/Idealogy.js'
+import Portfolio from './components/Portfolio.js';
+import Contact from './components/Contact.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
@@ -11,6 +11,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import './App.css';
 
 
 function App() {
@@ -36,19 +37,29 @@ function App() {
         </ul>
       </div>
       <div class="container-home">
-
+        <div className="stars-container">
+          <div class="stars"></div>
+          <div class="twinkling"></div>
+          <div class="clouds"></div>
+        </div>
+        <div className="intro-title">
+          <div>
+            <p className="title-phrase">Hello, I'm <span className="title">Christopher Brum</span></p>
+            <p className="title-phrase">I'm a Full Stack Developer</p>
+          </div>
+        </div>
       </div>
       <div class="container-idealogy">
-
+       
       </div>
       <div class="container-portfolio">
-
+        
       </div>
       <div class="container-about">
-
+        
       </div>
       <div class="container-contact">
-
+        
       </div>
     </div>
   );
