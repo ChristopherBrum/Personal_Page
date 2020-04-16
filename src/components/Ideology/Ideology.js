604fc63ecc43d8a1461781f411d05ce93a83931c
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-04-16 16:11:23 -0500

    icoons added to shapes, text placed roughly

--- src/components/Ideology/Ideology.js
+++ src/components/Ideology/Ideology.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Ideology.css';
-import { TrophyFilled } from '@ant-design/icons';
+import { TrophyFilled, 
+         FireFilled, 
+         EyeFilled, 
+         SafetyCertificateFilled,
+         ThunderboltFilled } 
+from '@ant-design/icons';
 
 export default function Ideology(){
   return(
@@ -11,11 +16,45 @@ export default function Ideology(){
           <div className="tri-one" ></div>
           <TrophyFilled id="trophy-icon" />
         </div>
-        <div className="tri-two" ></div>
-        <div className="tri-three" ></div>
-        <div className="tri-four" ></div>
-        <div className="tri-five" ></div>
+        <div className="shape-div2">
+          <div className="tri-two" ></div>
+          <FireFilled id="fire-icon" />
+        </div>
+        <div className="shape-div3">
+          <div className="tri-three" ></div>
+          <EyeFilled id="eye-icon" />
+        </div>
+        <div className="shape-div4">
+          <div className="tri-four" ></div>
+          <SafetyCertificateFilled id="shield-icon" />
+        </div>
+        <div className="shape-div5">
+          <div className="tri-five" ></div>
+          <ThunderboltFilled id="thunder-icon" />
+        </div>
       </div>
+        <div className="ideology-text-div">
+          <section className="id-section">
+            <h3 className="descrip-title">Build With Quality</h3>
+            <p className="descrip">Write efficient, testable and scalable code by adhering to best software development practices</p>
+          </section>
+          <section className="upper-id-section">
+            <h3 className="descrip-title">Simple & Elegant</h3>
+            <p className="descrip">Write efficient, testable and scalable code by adhering to best software development practices</p>
+          </section>
+          <section className="id-section">
+            <h3 className="descrip-title">User First</h3>
+            <p className="descrip">Write efficient, testable and scalable code by adhering to best software development practices</p>
+          </section>
+          <section className="upper-id-section">
+            <h3 className="descrip-title">Secure</h3>
+            <p className="descrip">Write efficient, testable and scalable code by adhering to best software development practices</p>
+          </section>
+          <section className="id-section">
+            <h3 className="descrip-title">Speed</h3>
+            <p className="descrip">Write efficient, testable and scalable code by adhering to best software development practices</p>
+          </section>
+        </div>
     </div>
   )
 }
