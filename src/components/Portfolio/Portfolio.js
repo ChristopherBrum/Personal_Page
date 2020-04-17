18582f5b6838a272e13cbb170cef21c43de85873
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-04-17 15:31:04 -0500

    hexagon shape color and layout

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,11 +4,30 @@ import './Portfolio.css';
 export default function Portfolio(){
     return(
       <div className="hex-container">
-        <div id="hex1"></div>        
+        <div id="hex1" class="hexagon-wrapper">
+            <div id="color1" class="hexagon"></div>
+        </div>
+        <div id="hex2" class="hexagon-wrapper">
+            <div id="color2" class="hexagon"></div>
+        </div>
+        <div id="hex3" class="hexagon-wrapper">
+            <div id="color3" class="hexagon"></div>
+        </div>
+        <div id="hex4" class="hexagon-wrapper">
+            <div id="color2" class="hexagon"></div>
+        </div>
+        <div id="hex5" class="hexagon-wrapper">
+            <div id="color1" class="hexagon"></div>
+        </div>
+        
+        
+        
+        
+        {/* <div id="hex1"></div>        
         <div id="hex2"></div>
         <div id="hex3"></div>
         <div id="hex4"></div>
-        <div id="hex5"></div>
+        <div id="hex5"></div> */}
       </div>
     )
 }
