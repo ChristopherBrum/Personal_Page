8dbc51de874b4d1ca421628df06042c8ea8d672a
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-04-17 17:03:53 -0500

    app logos added over hexagons

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -10,25 +10,21 @@ export default function Portfolio(){
         </div>
         <div id="hex2" class="hexagon-wrapper">
             <div id="color2" class="hexagon"></div>
+            <img src="/cobrawhite.png" className="colabras-logo" alt="logo for Colabras Game app" />
+            <h3 className="descrip-title">Colabras Game</h3>
+            <p className="descrip">Colabras is a mind game using words and colors built in React.js</p>
         </div>
         <div id="hex3" class="hexagon-wrapper">
             <div id="color3" class="hexagon"></div>
+            <img src="/taco-white.png" className="taco-logo" alt="logo for Taco To Yousday app" />
         </div>
         <div id="hex4" class="hexagon-wrapper">
             <div id="color2" class="hexagon"></div>
+            <img src="/moto-man.png" className="moto-logo" alt="logo for Two Wheeled Buy & Sell app" />
         </div>
         <div id="hex5" class="hexagon-wrapper">
             <div id="color1" class="hexagon"></div>
         </div>
-        
-        
-        
-        
-        {/* <div id="hex1"></div>        
-        <div id="hex2"></div>
-        <div id="hex3"></div>
-        <div id="hex4"></div>
-        <div id="hex5"></div> */}
       </div>
     )
 }
