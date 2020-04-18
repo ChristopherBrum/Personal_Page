76f1560ea74fc12024968588e1fa1d462bc1236a
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-04-18 11:45:24 -0500

    readjusted circle shapes

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,14 +9,14 @@ export default function AboutMe(props){
                 <img src="/chris_pic.png" className="pic-logo" alt="logo" />
                 <div className="circle1"></div>
                 <div className="circle2"></div>
-                <div className="circle2A"></div>
-                <div className="circle2B"></div>
+                <div className="circle2-top"></div>
+                <div className="circle2-bottom"></div>
                 <div className="circle3"></div>
-                <div className="circle3A"></div>
-                <div className="circle3B"></div>
+                <div className="circle3-top"></div>
+                <div className="circle3-bottom"></div>
                 <div className="circle4"></div>
-                <div className="circle4A"></div>
-                <div className="circle4B"></div>
+                <div className="circle4-top"></div>
+                <div className="circle4-bottom"></div>
                 <div className="circle5"></div>
             </div>
         </div>
