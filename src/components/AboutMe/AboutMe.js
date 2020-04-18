c34c3b67f22a42e5d32eeb1cd3d6419b52b8e88e
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-04-17 19:03:53 -0500

    styled circles and img in about section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -4,12 +4,19 @@ import './AboutMe.css';
 export default function AboutMe(props){
     return(
         <div className="-about-div">
+            <h2 className="about-title" >About Me</h2>
             <div className="circle-container">
                 <img src="/chris_pic.png" className="pic-logo" alt="logo" />
                 <div className="circle1"></div>
                 <div className="circle2"></div>
+                <div className="circle2A"></div>
+                <div className="circle2B"></div>
                 <div className="circle3"></div>
+                <div className="circle3A"></div>
+                <div className="circle3B"></div>
                 <div className="circle4"></div>
+                <div className="circle4A"></div>
+                <div className="circle4B"></div>
                 <div className="circle5"></div>
             </div>
         </div>
