5b2e5561dde827ad22d0027977be828a0dbd833f
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-04-21 16:31:14 -0500

    contact div styled

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './Contact.css';
-import { VerticalAlignTopOutlined } from '@ant-design/icons';
 
 export default function Contact(){
     return(
-      <div>
-        
+      <div className="main-col-container">
+        <h2 className="contact-title">Contact</h2>
+        <div className="column-container">
+          <div className="col-x"></div>
+          <div className="col-a"></div>
+          <div className="col-b"></div>
+          <div className="col-c"></div>
+          <div className="col-d"></div>
+          <div className="col-e"></div>
+          <div className="col-d"></div>
+          <div className="col-c"></div>
+          <div className="col-b"></div>
+          <div className="col-a"></div>
+          <div className="col-x"></div>
+          <div className="col-a"></div>
+          <div className="col-b"></div>
+          <div className="col-c"></div>
+          <div className="col-d"></div>
+          <div className="col-e"></div>
+          <div className="col-d"></div>
+          <div className="col-c"></div>
+          <div className="col-b"></div>
+          <div className="col-a"></div>
+          <div className="col-x"></div>
+        </div>
+        <div className="">
+          <h3>Send me a messeage</h3>
+        </div>
       </div>
     )
 }
