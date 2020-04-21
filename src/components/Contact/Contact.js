ae3b919d07b5c65c6e7de8a39b2d086f8bd432b3
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-04-21 17:42:27 -0500

    social icons added and up button to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Contact.css';
+import { GithubFilled, TwitterCircleFilled, CodepenCircleFilled, LinkedinFilled, UpSquareOutlined } from '@ant-design/icons';
 
 export default function Contact(){
     return(
       <div className="main-col-container">
-        <h2 className="contact-title">Contact</h2>
+        <h2 className="contact-title">Get in Touch</h2>
         <div className="column-container">
           <div className="col-x"></div>
           <div className="col-a"></div>
@@ -35,6 +36,15 @@ export default function Contact(){
             <label for="message-input" placeholder="Enter you message..." required>Send me a message:</label>
             <textarea id="user-message" type="text" placeholder="Enter you message..."></textarea>
           </div>
+        </div>
+        <div id="social-icons">
+          <a href="https://www.linkedin.com/in/chris-brum/" target="_blank" ><LinkedinFilled /></a>
+          <a href="https://github.com/ChristopherBrum" target="_blank" ><GithubFilled /></a>
+          <a href="https://twitter.com/_B_R_U_M_" target="_blank" ><TwitterCircleFilled /></a>
+          <a href="https://codepen.io/christopherbrum" target="_blank" ><CodepenCircleFilled /></a>
+        </div>
+        <div id="up-icon">
+          <a href="#container-home"><UpSquareOutlined /></a>
         </div>
       </div>
     )
